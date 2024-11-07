729f67ea2e726961bd7d8779d988fb672e6efb3b
Marcap00 <cappellatomarco@yahoo.com>
2024-11-07 17:31:16 +0100

    refactoring show page and added technologies badge

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -5,10 +5,22 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="card text-center">
-                    <div class="card-body">
-                        <p class="card-text">Category: {{ $project->type->name }}</p>
-                        <h5 class="card-title"> {{ $project->author }} </h5>
-                        <h6 class="card-subtitle mb-2 text-body-secondary"> {{ $project->title }} </h6>
+                    <div class="card-img-top">
+                        <img class=" rounded-3 img-fluid" src="{{"https://placehold.co/400x300?text=" . $project->title }}" alt="{{ $project->name }}">
+                    </div>
+                    <div class="card-body flex justify-content-center">
+                        <p class="card-text">Category: <span class="fw-semibold">{{ $project->type->name }}</span></p>
+                        <p class="card-text">
+                            @forelse  ($project->technologies as $technology)
+                            <span class="badge text-black" style="background-color: {{ $technology->color }}">
+                                {{ $technology->name }}
+                            </span>
+                            @empty
+                                No Technologies
+                            @endforelse
+                        </p>
+                        <h3 class="card-title mb-3"> {{ $project->title }} </h3>
+                        <h6 class="card-subtitle mb-2 text-body-secondary"> {{ $project->author }} </h6>
                         <p class="card-text"> {{ $project->description }} </p>
                         <a href="{{ route('admin.projects.index') }}" title="Back"><button class="btn btn-secondary me-2">Back <i class="fa fa-arrow-left"></i></button></a>
                         <a href="{{ route('admin.projects.edit', $project) }}"><button class="btn btn-warning">Edit <i class="fa fa-pencil"></i></button></a>
