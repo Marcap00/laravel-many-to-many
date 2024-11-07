d3d9ec2d0e018530a64814f15e5cf54dbd77cc05
Marcap00 <cappellatomarco@yahoo.com>
2024-11-07 19:49:12 +0100

    implemented all methods technologyController

--- app/Http/Controllers/Admin/TechnologyController.php
+++ app/Http/Controllers/Admin/TechnologyController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreTechnologyRequest;
+use App\Http\Requests\UpdateTechnologyRequest;
+use App\Models\Project;
 use App\Models\Technology;
 use Illuminate\Http\Request;
 
@@ -16,44 +19,99 @@ class TechnologyController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index() {}
+    public function index()
+    {
+        $technologies = Technology::paginate(10);
+
+        return view('admin.technologies.layouts.table', compact('technologies'));
+    }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create() {}
+    public function create()
+    {
+        $technology = new Technology();
+        $projects = Project::all();
+
+        return view('admin.technologies.layouts.form', compact('technology', 'projects'));
+    }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request) {}
+    public function store(StoreTechnologyRequest $request)
+    {
+
+        $data = $request->validated();
+
+        $technology = Technology::create($data);
+
+        return redirect()->route('admin.technologies.show', compact('technology'));
+    }
 
     /**
      * Display the specified resource.
      */
-    public function show(Technology $technology) {}
+    public function show(Technology $technology)
+    {
+        return view('admin.technologies.show', compact('technology'));
+    }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Technology $technology) {}
+    public function edit(Technology $technology)
+    {
+        $projects = Project::all();
+
+        return view('admin.technologies.layouts.form', compact('technology', 'projects'));
+    }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Technology $technology) {}
+    public function update(UpdateTechnologyRequest $request, Technology $technology)
+    {
+        $data = $request->validated();
+
+        $technology->update($data);
+
+        return redirect()->route('admin.technologies.show', $technology);
+    }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Technology $technology) {}
+    public function destroy(Technology $technology)
+    {
+        $technology->delete();
+
+        return redirect()->route('admin.technologies.index');
+    }
 
     /**
      * Restore the specified resource from storage.
      */
-    public function restore(string $id) {}
+    public function restore(string $id)
+    {
+        $technology = Technology::onlyTrashed()->findorFail($id);
+        $technology->restore();
+        return redirect()->route('admin.technologies.bin');
+    }
 
-    public function permanentDestroy(string $id) {}
+    public function permanentDestroy(string $id)
+    {
+        $technology = Technology::onlyTrashed()->findorFail($id);
+        $technology->forceDelete();
 
-    public function bin() {}
+        return redirect()->route('admin.technologies.bin');
+    }
+
+    public function bin()
+    {
+        $technologies = Technology::onlyTrashed()->paginate(10);
+
+        return view('admin.technologies.layouts.table', compact('technologies'));
+    }
 }
