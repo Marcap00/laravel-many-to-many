1183bc1a9b7dd6d56ce4f8648ed006e62d195fd3
Marcap00 <cappellatomarco@yahoo.com>
2024-11-07 23:36:59 +0100

    updated ProjectTechnologySeeder

--- database/seeders/ProjectTechnologySeeder.php
+++ database/seeders/ProjectTechnologySeeder.php
@@ -7,6 +7,7 @@ use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 class ProjectTechnologySeeder extends Seeder
 {
@@ -21,7 +22,7 @@ class ProjectTechnologySeeder extends Seeder
         // All IDs Technologies
         $technologies = Technology::all()->pluck('id');
 
-        $projects[0]->technologies()->sync([6]);
+        /* $projects[0]->technologies()->sync([6]);
         $projects[1]->technologies()->sync([6]);
         $projects[2]->technologies()->sync([6]);
         $projects[3]->technologies()->sync([5]);
@@ -33,6 +34,23 @@ class ProjectTechnologySeeder extends Seeder
         $projects[9]->technologies()->sync([4]);
         $projects[10]->technologies()->sync([1]);
         $projects[11]->technologies()->sync([1]);
-        $projects[12]->technologies()->sync([3]);
+        $projects[12]->technologies()->sync([3]); */
+
+        foreach ($projects as $project) {
+            $formattedName = str_ireplace('-', ' ', $project->title);
+            if (Str::contains($formattedName, 'laravel')) {
+                $project->technologies()->sync([6]);
+            } elseif (Str::contains($formattedName, ['vite', 'vue'])) {
+                $project->technologies()->sync([5]);
+            } elseif (Str::contains($formattedName, 'html')) {
+                $project->technologies()->sync([1]);
+            } elseif (Str::contains($formattedName, 'css')) {
+                $project->technologies()->sync([2]);
+            } elseif (Str::contains($formattedName, ['js', 'javascript'])) {
+                $project->technologies()->sync([3]);
+            } elseif (Str::contains($formattedName, 'php')) {
+                $project->technologies()->sync([4]);
+            }
+        }
     }
 }
