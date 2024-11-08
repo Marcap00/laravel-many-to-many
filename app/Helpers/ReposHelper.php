f0076a7222139217241808fd90a33dcda14ab34b
Marcap00 <cappellatomarco@yahoo.com>
2024-11-08 17:06:29 +0100

    update of last commit

--- app/Helpers/ReposHelper.php
+++ app/Helpers/ReposHelper.php
@@ -11,11 +11,10 @@ class ReposHelper
     public static function getRepos()
     {
         $username = 'Marcap00';
-        $response = Http::withToken(env('GIT_HUB_ACCESS_TOKEN'))->get("https://api.github.com/repos/$username");
-        if ($response->failed()) {
-            throw new FileNotFoundException('Invalid or path not found');
-        } else {
-            return $response;
-        }
+        $response = Http::withToken(env('GIT_HUB_ACCESS_TOKEN'))
+            ->withOptions(['verify' => false])
+            ->get("https://api.github.com/repos/" . $username);
+
+        return $response;
     }
 }
