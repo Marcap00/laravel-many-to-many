0b4e0590bc0aa85ac4831bc3ea0d53788c2d8b41
Marcap00 <cappellatomarco@yahoo.com>
2024-11-13 22:25:27 +0100

    added filepath in to request's data image

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -9,6 +9,7 @@ use App\Models\Project;
 use App\Models\Technology;
 use App\Models\Type;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ProjectController extends Controller
 {
@@ -46,6 +47,9 @@ class ProjectController extends Controller
     public function store(StoreProjectRequest $request)
     {
         $data = $request->validated();
+
+        $filePath = Storage::disk("public")->put("img/projects/", $request->image);
+        $data['image'] = $filePath;
 
         $project = Project::create($data);
 
